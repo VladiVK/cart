a75256d6148a39e20de15758478a37d8fed8ef45
VladiVK <kozobon@gmail.com>
2020-12-07 22:08:22 +0200

    basic functionality of reducer

--- src/context.js
+++ src/context.js
@@ -13,13 +13,53 @@ const initialState = {
     total: 0,
     amount: 0,
 }
+
+// Types 
+const CLEAR_CART = 'CLEAR_CART';
+const REMOVE_ITEM = 'REMOVE_ITEM';
+const INCREASE = 'INCREASE'
+const DECREASE = 'DECREASE'
+const GET_TOTAL = 'GET_TOTAL'
+
 const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState)
+
+  const clearCart = () => {
+    dispatch({type: CLEAR_CART})
+  }
+  const removeItem = (id) => {
+    dispatch({
+      type: REMOVE_ITEM,
+      payload: id,
+    })
+  }
+  const increase = (id) => {
+    dispatch({
+      type: INCREASE,
+      payload: id,
+    })
+  }
+  const decrease = (id) => {
+    dispatch({
+      type: DECREASE,
+      payload: id,
+    })
+  }
+  useEffect( () => {
+    dispatch({
+      type: GET_TOTAL
+    })
+  }, [state.cart]);
+
 
   return (
     <AppContext.Provider
       value={{
         ...state,
+        clearCart,
+        removeItem,
+        increase,
+        decrease,
       }}
     >
       {children}
--- src/reducer.js
+++ src/reducer.js
@@ -1,7 +1,55 @@
 
 
 const reducer = (state, action) => {
-    return state;
+
+    switch (action.type) {
+        case 'CLEAR_CART': return {
+            ...state,
+            cart: [],
+        }
+        case 'REMOVE_ITEM': return {
+            ...state,
+            cart: state.cart.filter( item => item.id !== action.payload),
+        }
+        case 'INCREASE': return {
+            ...state,
+            cart: state.cart.map( item => {
+                return (item.id === action.payload) ? {...item, amount: item.amount + 1} : item;
+            }),
+        }
+        case 'DECREASE': return {
+            ...state,
+            cart: state.cart.map( item => {
+                if(item.id === action.payload) {
+                    return {...item, amount: item.amount - 1}
+                }
+                return item;
+                // а теперь исчезнет товар из списка корзины, если отняли все до 0 
+            }).filter( item => item.amount !== 0)
+        }
+        case 'GET_TOTAL': 
+        // complex reduce
+            let {total, amount} = state.cart.reduce( (cartTotal, cartItem) => {
+
+                const {price, amount} = cartItem;
+                const itemTotal = price * amount;
+
+                cartTotal.total += itemTotal;
+                cartTotal.amount += amount;
+
+                return cartTotal
+            }, {
+                total: 0,
+                amount: 0,
+            });
+
+            total = parseFloat(total.toFixed(2));
+
+            return {...state, total, amount};
+            
+    
+        default: return state
+    }
 
 }
 
